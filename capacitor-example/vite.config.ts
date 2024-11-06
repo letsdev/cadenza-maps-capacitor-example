71deced578af61e5444ce0c0437aa3083eb91fa6
Marc Mautz <m.mautz@herzogkommunikation.de>
2024-11-06 14:07:55 +0100

    added env file with multiple env's for mobile server base url & credentials, enabled env's in vite configuration

--- capacitor-example/vite.config.ts
+++ capacitor-example/vite.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  root: './src',
-  build: {
-    outDir: '../dist',
-    minify: false,
-    emptyOutDir: true,
-  },
-});
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    root: './src',
+    build: {
+      outDir: '../dist',
+      minify: false,
+      emptyOutDir: true
+    },
+    define: {
+      CMS_ACCOUNT_PW: JSON.stringify(env.WTP_CMS_ACCOUNT_PW),       // or use "WP_CMS_ACCOUNT_PW" from env file
+      CMS_ACCOUNT_KEY: JSON.stringify(env.WTP_CMS_ACCOUNT_KEY),     // or use "WP_CMS_ACCOUNT_KEY" from env file
+      CMS_BASE_URL: JSON.stringify(env.WTP_CMS_BASE_URL),           // or use "WP_CMS_BASE_URL" from env file
+      DOWNLOADABLE_MAP_ID: JSON.stringify(env.DOWNLOADABLE_MAP_ID)  // NOTE: downloadable map id, depending on mobile server environment "WTP_CMS_BASE_URL" or "WP_CMS_BASE_URL"
+    }
+  }
+})
